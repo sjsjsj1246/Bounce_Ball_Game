4581e2acb5959305a9df3793a31348ec5cd58421
Hwang In Seo <sjsjsj1246@gmail.com>
2019-07-17 15:04:40 +0900

    update

---
+++ project/Bounce_Ball/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    public float speed = 0.5f;
+    Transform playerTf;
+    Vector3 playerPos;
+
+    void Awake () {
+        playerTf = transform;
+    }
+
+    void Start () {
+        playerPos = playerTf.position;
+    }
+
+    void FixedUpdate () {
+        playerPos.x += speed;
+        playerTf.position = playerPos;
+    }
+}
